21ac74f9240e61feef6771ca9ccfe09722b27475
sametorbuk <orbuk60@gmail.com>
2024-06-29 15:37:55 +0300

    düzenleme

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ subscriberScreen.style.top = "2rem"
 subscriberScreen.style.width = "40rem"
 subscriberScreen.style.height = "33rem"
 subscriberScreen.style.flexDirection = "column"
+subscriberScreen.style.borderRadius = "15px"
 
 const subscriberScreenCloseBtn = document.createElement("button")
 subscriberScreenCloseBtn.textContent = "X"
@@ -83,34 +84,41 @@ subscriberScreen.appendChild(subscriberScreenOptionsDiv)
 subscriberScreenOptionsDiv.style.display = "flex"
 subscriberScreenOptionsDiv.style.justifyContent = "space-around"
 
-subscriberScreenOptionsDiv.style.background = "white"
+subscriberScreenOptionsDiv.style.background = "black"
 subscriberScreenOptionsDiv.style.height = "13rem"
 subscriberScreenOptionsDiv.style.wi = "40rem"
-
 
 const subscriberScreenOptionsDivLeft = document.createElement("div")
 subscriberScreenOptionsDiv.appendChild(subscriberScreenOptionsDivLeft)
 subscriberScreenOptionsDivLeft.style.display = "flex"
 subscriberScreenOptionsDivLeft.style.flexDirection = "column"
+subscriberScreenOptionsDivLeft.style.border = "1px solid gray"
+subscriberScreenOptionsDivLeft.style.borderRadius = "1rem"
+
+
 
 const subscriberScreenOptionsDivLeftp1 = document.createElement("p")
 subscriberScreenOptionsDivLeft.appendChild(subscriberScreenOptionsDivLeftp1)
 subscriberScreenOptionsDivLeftp1.textContent = "Premium"
 subscriberScreenOptionsDivLeftp1.style.color = "gray"
+subscriberScreenOptionsDivLeftp1.style.marginTop = "4rem"
+subscriberScreenOptionsDivLeftp1.style.marginBottom = "0"
+subscriberScreenOptionsDivLeftp1.style.paddingLeft="1rem"
 
-const subscriberScreenOptionsDivLeftp2 = document.createElement("p")
+const subscriberScreenOptionsDivLeftp2 = document.createElement("h3")
 subscriberScreenOptionsDivLeft.appendChild(subscriberScreenOptionsDivLeftp2)
 subscriberScreenOptionsDivLeftp2.textContent = "Bireyim"
-subscriberScreenOptionsDivLeftp2.style.color = "gray"
+subscriberScreenOptionsDivLeftp2.style.color = "white"
 subscriberScreenOptionsDivLeftp2.style.fontWeight = "bold"
+subscriberScreenOptionsDivLeftp2.style.margin = "0"
+subscriberScreenOptionsDivLeftp2.style.paddingLeft="1rem"
 
 const subscriberScreenOptionsDivLeftp3 = document.createElement("p")
 subscriberScreenOptionsDivLeft.appendChild(subscriberScreenOptionsDivLeftp3)
 subscriberScreenOptionsDivLeftp3.textContent = "Bireyler ve içerik üreticileri için"
 subscriberScreenOptionsDivLeftp3.style.color = "gray"
-subscriberScreenOptionsDivLeftp3.style.fontWeight = "bold"
-
-
+subscriberScreenOptionsDivLeftp3.style.paddingLeft="1rem"
+subscriberScreenOptionsDivLeftp3.style.margin = "0"
 
 
 
@@ -118,23 +126,117 @@ const subscriberScreenOptionsDivRight = document.createElement("div")
 subscriberScreenOptionsDiv.appendChild(subscriberScreenOptionsDivRight)
 subscriberScreenOptionsDivRight.style.display = "flex"
 subscriberScreenOptionsDivRight.style.flexDirection = "column"
+subscriberScreenOptionsDivRight.style.width = "14rem"
+subscriberScreenOptionsDivRight.style.border = "1px solid gray"
+subscriberScreenOptionsDivRight.style.borderRadius = "1rem"
+
+subscriberScreenOptionsDivRight.addEventListener("mouseenter" , (event) => {
+  subscriberScreenOptionsDivRight.style.background = "#8080805e"
+})
+
+subscriberScreenOptionsDivRight.addEventListener("mouseleave" , (event) => {
+  subscriberScreenOptionsDivRight.style.background = "black"
+})
+
+subscriberScreenOptionsDivLeft.addEventListener("mouseenter" , (event) => {
+  subscriberScreenOptionsDivLeft.style.background = "#8080805e"
+})
+
+subscriberScreenOptionsDivLeft.addEventListener("mouseleave" , (event) => {
+  subscriberScreenOptionsDivLeft.style.background = "black"
+})
 
 const subscriberScreenOptionsDivRightp1 = document.createElement("p")
 subscriberScreenOptionsDivRight.appendChild(subscriberScreenOptionsDivRightp1)
 subscriberScreenOptionsDivRightp1.textContent = "Onaylı kuruluşlar"
 subscriberScreenOptionsDivRightp1.style.color = "gray"
+subscriberScreenOptionsDivRightp1.style.marginTop = "3rem"
+subscriberScreenOptionsDivRightp1.style.marginBottom = "0"
+subscriberScreenOptionsDivRightp1.style.paddingLeft="1rem"
 
-const subscriberScreenOptionsDivRightp2 = document.createElement("p")
+const subscriberScreenOptionsDivRightp2 = document.createElement("h3")
 subscriberScreenOptionsDivRight.appendChild(subscriberScreenOptionsDivRightp2)
 subscriberScreenOptionsDivRightp2.textContent = "Kuruluşum"
-subscriberScreenOptionsDivRightp2.style.color = "gray"
+subscriberScreenOptionsDivRightp2.style.color = "white"
 subscriberScreenOptionsDivRightp2.style.fontWeight = "bold"
+subscriberScreenOptionsDivRightp2.style.margin = "0"
+subscriberScreenOptionsDivRightp2.style.paddingLeft="1rem"
 
 const subscriberScreenOptionsDivRightp3 = document.createElement("p")
 subscriberScreenOptionsDivRight.appendChild(subscriberScreenOptionsDivRightp3)
 subscriberScreenOptionsDivRightp3.textContent = "İşletmeler,devlet kurumları, kar amacı gütmeyen kuruluşlar için"
 subscriberScreenOptionsDivRightp3.style.color = "gray"
-subscriberScreenOptionsDivRightp3.style.fontWeight = "bold"
+subscriberScreenOptionsDivRightp3.style.paddingLeft="1rem"
+subscriberScreenOptionsDivRightp3.style.margin = "0"
+
+subscriberScreenOptionsDivLeft.addEventListener("mousedown" , (event) => {
+  subscriberScreenOptionsDivLeft.style.border = "2px solid #4798ed"
+})
+
+subscriberScreenOptionsDivLeft.addEventListener("mouseup" , (event) => {
+  subscriberScreenOptionsDivLeft.style.border = "1px solid gray"
+})
+
+subscriberScreenOptionsDivRight.addEventListener("mousedown" , (event) => {
+  subscriberScreenOptionsDivRight.style.border = "2px solid #4798ed"
+})
+
+subscriberScreenOptionsDivRight.addEventListener("mouseup" , (event) => {
+  subscriberScreenOptionsDivRight.style.border = "1px solid gray"
+})
 
 
+const subscriberScreenSubscribeBtn = document.createElement("button")
+subscriberScreen.appendChild(subscriberScreenSubscribeBtn)
+subscriberScreenSubscribeBtn.classList.add("white-subscribe-btn")
+subscriberScreenSubscribeBtn.textContent =  "Abone ol"
 
+const subscriberScreenMoreInfo = document.createElement("div")
+subscriberScreen.appendChild(subscriberScreenMoreInfo)
+subscriberScreenMoreInfo.classList.add("subscribe-info")
+
+const subscriberScreenMoreInfop = document.createElement("p")
+subscriberScreenMoreInfop.textContent = "Daha fazla bilgi al:"
+subscriberScreenMoreInfop.style.color = "white"
+subscriberScreenMoreInfo.appendChild(subscriberScreenMoreInfop)
+subscriberScreenMoreInfop.style.marginLeft = "10rem"
+
+const moreInfoPremiumBtn = document.createElement("button")
+subscriberScreenMoreInfo.appendChild(moreInfoPremiumBtn)
+moreInfoPremiumBtn.classList.add("more-info-premium-btn")
+moreInfoPremiumBtn.textContent = "Premium"
+
+
+moreInfoPremiumBtn.addEventListener("click" , (event) => {
+  window.open("https://help.x.com/tr/using-x/x-premium" , "_blank")  
+})
+
+moreInfoPremiumBtn.addEventListener("mouseenter" , (event) => {
+  moreInfoPremiumBtn.style.textDecoration = "1px solid blue"
+})
+
+moreInfoPremiumBtn.addEventListener("mouseleave" , (event) => {
+  moreInfoPremiumBtn.style.textDecoration = "none"
+})
+
+
+const subscriberScreenMoreInfop2 = document.createElement("p")
+subscriberScreenMoreInfop2.textContent = "ve"
+subscriberScreenMoreInfop2.style.color = "white"
+subscriberScreenMoreInfo.appendChild(subscriberScreenMoreInfop2)
+subscriberScreenMoreInfop2.style.position = "relative"
+subscriberScreenMoreInfop2.style.right = "1rem"
+
+const moreInfoAcceptEstablishmentBtn = document.createElement("button")
+subscriberScreenMoreInfo.appendChild(moreInfoAcceptEstablishmentBtn)
+moreInfoAcceptEstablishmentBtn.classList.add("more-info-premium-btn2")
+moreInfoAcceptEstablishmentBtn.textContent = "Onaylı Kuruluşlar"
+
+moreInfoAcceptEstablishmentBtn.addEventListener("click" , (event) => {
+  window.open("https://help.x.com/using-twitter/verified-organizations" , "_blank")
+})
+
+
+subscriberScreenSubscribeBtn.addEventListener("click" , (event) => {
+  window.open("https://x.com/i/premium_sign_up" , "_blank")
+})
